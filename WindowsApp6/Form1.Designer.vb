882b24fa6537a381344f8d716478f92989220e7b
ismael leal <i42lecai@uco.es>
2019-08-19 01:32:39 +0100

    Agregar archivos de proyecto.

---
+++ WindowsApp6/Form1.Designer.vb
@@ -0,0 +1,324 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.BindingSource1 = New System.Windows.Forms.BindingSource(Me.components)
+        Me.BindingSource2 = New System.Windows.Forms.BindingSource(Me.components)
+        Me.ASM = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Button4 = New System.Windows.Forms.Button()
+        CType(Me.BindingSource1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.BindingSource2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ASM
+        '
+        Me.ASM.AutoSize = True
+        Me.ASM.Location = New System.Drawing.Point(119, 35)
+        Me.ASM.Name = "ASM"
+        Me.ASM.Size = New System.Drawing.Size(30, 13)
+        Me.ASM.TabIndex = 0
+        Me.ASM.Text = "ASM"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(459, 39)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(32, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "WEB"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(132, 203)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "TEST"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(462, 203)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "TEST"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(228, 277)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 4
+        Me.Button3.Text = "CONECTAR"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"5", "10", "15", "20", "25", "30", "35", "40", "45", "50", "55", "60", "90", "120", "240"})
+        Me.ComboBox1.Location = New System.Drawing.Point(228, 353)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox1.TabIndex = 5
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(245, 303)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Label1"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(149, 187)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(39, 13)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Label3"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(491, 187)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(39, 13)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "Label4"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(122, 79)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 9
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(122, 115)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 10
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(122, 155)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox3.TabIndex = 11
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(36, 82)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(63, 13)
+        Me.Label5.TabIndex = 12
+        Me.Label5.Text = "SERVIDOR"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(36, 122)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(56, 13)
+        Me.Label6.TabIndex = 13
+        Me.Label6.Text = "USUARIO"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(36, 162)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(81, 13)
+        Me.Label7.TabIndex = 14
+        Me.Label7.Text = "CONTRASEÑA"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(376, 162)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(81, 13)
+        Me.Label8.TabIndex = 20
+        Me.Label8.Text = "CONTRASEÑA"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(376, 122)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(56, 13)
+        Me.Label9.TabIndex = 19
+        Me.Label9.Text = "USUARIO"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(376, 82)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(63, 13)
+        Me.Label10.TabIndex = 18
+        Me.Label10.Text = "SERVIDOR"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(462, 155)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox4.TabIndex = 17
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(462, 115)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox5.TabIndex = 16
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(462, 79)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox6.TabIndex = 15
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(41, 356)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(181, 13)
+        Me.Label11.TabIndex = 21
+        Me.Label11.Text = "INTERVALO DE SINCRONIZACION"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(355, 356)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(57, 13)
+        Me.Label12.TabIndex = 22
+        Me.Label12.Text = "MINUTOS"
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(462, 343)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(104, 38)
+        Me.Button4.TabIndex = 23
+        Me.Button4.Text = "SINCRONIZAR AHORA"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(605, 399)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.TextBox6)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.ASM)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.BindingSource1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.BindingSource2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents BindingSource1 As BindingSource
+    Friend WithEvents BindingSource2 As BindingSource
+    Friend WithEvents ASM As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Button4 As Button
+End Class
---
+++ WindowsApp6/Form1.vb
@@ -0,0 +1,176 @@
+﻿Public Class Form1
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles ASM.Click
+
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+
+
+        Dim con As New OleDb.OleDbConnection("Provider=Microsoft.Jet.Oledb.4.0; Data Source= D:\ismae\Documents\equipos1.1.mdb")
+
+        Dim cmd1 As New OleDb.OleDbCommand
+        Dim dr1 As OleDb.OleDbDataReader
+
+        Dim cmd2 As New OleDb.OleDbCommand
+        Dim dr2 As OleDb.OleDbDataReader
+
+
+        Dim cmd3 As New OleDb.OleDbCommand
+        Dim dr3 As OleDb.OleDbDataReader
+
+
+        Dim cmd4 As New OleDb.OleDbCommand
+        Dim dr4 As OleDb.OleDbDataReader
+
+        Dim cmd5 As New OleDb.OleDbCommand
+        Dim dr5 As OleDb.OleDbDataReader
+
+
+        'base de datos asm
+        Try
+            con.Open()
+            MsgBox("Conexión Exitosa con la base de datos", MsgBoxStyle.Information)
+
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+
+
+        'base de datos web
+
+        Public conweb As New OleDb.OleDbConnection("Provider=Microsoft.Jet.Oledb.4.0; Data Source= D:\ismae\Documents\equipos1.1.mdb")
+
+        Try
+            conweb.Open()
+            MsgBox("Conexión Exitosa con la base de datos", MsgBoxStyle.Information)
+
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+
+
+        'Clientes_datos 
+
+        cmd1.Connection = con
+        cmd1.CommandType = CommandType.Text
+        cmd1.CommandText = "SELECT Ges_Clientes.Codigo, Ges_Clientes.Nombre, Ges_Clientes.Nombre_Comercial, Ges_Clientes.Nif, Ges_Clientes.Direccion, Ges_Clientes.Poblacion, Ges_Clientes.Pais, Ges_Clientes.Email, Ges_Clientes.Telefono_1, Ges_Clientes.Telefono_2 FROM Ges_Clientes;"
+        dr1 = cmd1.ExecuteReader()
+
+
+
+
+        Dim Codigo_cliente As String
+        Dim Nombre As String
+        Dim Nombre_comercial As String
+        Dim Nif As String
+        Dim Direccion As String
+        Dim poblacion As String
+        Dim pais As String
+        Dim email As String
+        Dim telefono_1 As String
+        Dim telefono_2 As String
+
+
+
+
+
+        'Recepcion muestras
+
+        cmd2.Connection = con
+        cmd2.CommandType = CommandType.Text
+        cmd2.CommandText = "SELECT LMS_Recepcion.Muestra_Recepcion, LMS_Recepcion.Lote, LMS_Recepcion.Fecha_Recepcion, LMS_Recepcion.Referencia, LMS_Recepcion.Cliente, LMS_Recepcion.Quien_Trae_Muestra, LMS_Recepcion.Prioridad, LMS_Recepcion.Observaciones, LMS_Recepcion.ClienteFacturacion, LMS_Recepcion.Revision, LMS_Recepcion.Fecha_Revision FROM LMS_Recepcion;"
+        dr2 = cmd2.ExecuteReader()
+
+
+        Dim Muestra_Recepcion As String
+        Dim Lote As String
+        Dim Fecha_Recepcion As String
+        Dim Referencia As String
+        Dim cliente As String
+        Dim quien As String
+        Dim prioridad As String
+        Dim observaciones As String
+        Dim Cliente_facturacion As Boolean
+        Dim revision As Boolean
+        Dim fecha_revision As String
+
+
+
+        'Ensayos recepcion
+
+        cmd3.Connection = con
+        cmd3.CommandType = CommandType.Text
+        cmd3.CommandText = "SELECT LMS_Ensayos_Recepcion.Muestra_Recepcion, LMS_Ensayos_Recepcion.Ensayo, LMS_Ensayos_Recepcion.Grupo_Ensayos FROM LMS_Ensayos_Recepcion;"
+        dr3 = cmd3.ExecuteReader()
+
+
+
+        Dim Codigo As String = selecionada_equipos
+        Dim Denominacion As String
+        Dim Marca As String
+        Dim Modelo As String
+        Dim Estado As String
+        Dim Especificacion As String
+        Dim Serie As String
+        Dim software As String
+        Dim mantenimiento_calibracion As Boolean
+        Dim mantenimiento As Boolean
+        Dim ubicacion As String
+        Dim caracteristicas As String
+        Dim observaciones As String
+        Dim fecha_alta As String
+        Dim fecha_baja As String
+        'Ensayos
+
+        cmd4.Connection = con
+        cmd4.CommandType = CommandType.Text
+        cmd4.CommandText = "SELECT MES_Ensayos.Ensayo, MES_Ensayos.Nombre, MES_Ensayos.Metodo_Analitico, MES_Ensayos.Limite_Superior_Acreditacion, MES_Ensayos.Limite_Inferior_Acreditacion, MES_Ensayos.PNT FROM MES_Ensayos;"
+        dr4 = cmd4.ExecuteReader()
+
+        Dim Codigo As String = selecionada_equipos
+        Dim Denominacion As String
+        Dim Marca As String
+        Dim Modelo As String
+        Dim Estado As String
+        Dim Especificacion As String
+        Dim Serie As String
+        Dim software As String
+        Dim mantenimiento_calibracion As Boolean
+        Dim mantenimiento As Boolean
+        Dim ubicacion As String
+        Dim caracteristicas As String
+        Dim observaciones As String
+        Dim fecha_alta As String
+        Dim fecha_baja As String
+
+
+
+        cmd5.Connection = con
+        cmd5.CommandType = CommandType.Text
+        cmd5.CommandText = "Select Case LMS_Determinacion.Muestra_Recepcion, LMS_Determinacion.Ensayo, LMS_Determinacion.Rdo_Bruto, LMS_Determinacion.Atributo_Rdo, LMS_Determinacion.Rdo_Final From LMS_Determinacion;"
+        dr5 = cmd5.ExecuteReader()
+
+        Dim Codigo As String = selecionada_equipos
+        Dim Denominacion As String
+        Dim Marca As String
+        Dim Modelo As String
+        Dim Estado As String
+        Dim Especificacion As String
+        Dim Serie As String
+        Dim software As String
+        Dim mantenimiento_calibracion As Boolean
+        Dim mantenimiento As Boolean
+        Dim ubicacion As String
+        Dim caracteristicas As String
+        Dim observaciones As String
+        Dim fecha_alta As String
+        Dim fecha_baja As String
+
+
+    End Sub
+
+   
